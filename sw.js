769fcd64469a3c3a18d5c4892a56910ccfdde119
vijoci2 <vijoci2@gmail.com>
2026-09-15 05:54:48 +0530

    Refresh PWA cache for native alarm bridge update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pomodoro-3d-v1.5.0';
+const CACHE='pomodoro-3d-v1.6.0';
 const SHELL=['./','./index.html','./styles.css','./mechanical.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
